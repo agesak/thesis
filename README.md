d9cafaaa525a1aa3314e1b42a83c36016f3a2723
Kareha Agesa <agesak@uw.edu>
2019-11-12 15:25:36 -0800

    adding testing with sepsis data and rpart/tree testing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# thesis
-MPH Thesis - X59 and Y34 Garbage Code Redistribution
+# MPH Thesis Project - X59 and Y34 Garbage Code Redistribution
+## Author: Kareha Agesa
+## Anticipated Completion: Spring 2020
+
+This project contains code for my masters thesis on X59 and Y34 garbage code redistribution. It is dependent on data and infrastructure belonging to the Institute for Health Metrics and Evaluation (IHME).* Current structure is as follows:
+* `sepsis_testing` - an _unorganized_ R project using the sepsis data to experiment with the [tree package](https://cran.r-project.org/web/packages/tree/tree.pdf) 
+* `code` - code implementing machine learning and poisson methods
+
+
+-----
+* Is dependent on multiple cause of death data belonging to IHME, sources IHME central functions, and reads in files housed on IHME's cluster computing system.
---
+++ sepsis_testing/tree_test.R
@@ -0,0 +1,57 @@
+library(ISLR)
+library(tree)
+library(data.table)
+
+
+# test from: https://www.datacamp.com/community/tutorials/decision-trees-R
+carseats<-Carseats
+High = ifelse(carseats$Sales<=8, "No", "Yes")
+carseats = data.frame(carseats, High)
+
+tree.carseats = tree(High~.-Sales, data=carseats)
+summary(tree.carseats)
+plot(tree.carseats)
+text(tree.carseats, pretty = 0)
+
+
+# sepsis data
+df <- fread("all_locations_redistributed.csv")
+# df <- fread("data_subset.csv")
+df$cases_explicit[is.na(df$cases_explicit)] <- 0
+df$cases_implicit[is.na(df$cases_implicit)] <- 0
+df$deaths_explicit[is.na(df$deaths_explicit)] <- 0
+df$deaths_implicit[is.na(df$deaths_implicit)] <- 0
+
+
+df[, sepsis_cases := cases_implicit + cases_explicit]
+df[, sepsis_deaths := deaths_implicit + deaths_explicit]
+df[, sepsis_cfr := sepsis_deaths/sepsis_cases]
+
+df[, c("sex_id", "age_group_id", "level_1",
+         "level_2") := lapply(.SD, as.factor), .SDcols = c(
+           "sex_id", "age_group_id", "level_1", "level_2")]
+group_cols = c('sex_id', 'age_group_id', 'haqi', 'location_id', 'year_id', 'level_1', 'level_2')
+df[, c("sex_id", "age_group_id") := lapply(.SD, as.factor), .SDcols = c("sex_id", "age_group_id")]
+
+
+df = df[, lapply(.SD,sum), 
+        by=group_cols, 
+        .SDcols="sepsis_cfr"]
+df[,haqi_mean_scaled:=haqi/100]
+
+tree.sepsis = tree(sepsis_cfr~age_group_id+sex_id+haqi+level_2, df)
+# tree.sepsis = tree(sepsis_cfr~age_group_id+sex_id+haqi_mean_scaled, df)
+summary(tree.sepsis)
+plot(tree.sepsis)
+text(tree.sepsis, pretty = 0)
+
+set.seed(101)
+train=sample(1:nrow(df), 250)
+tree.sepsis_train = tree(sepsis_cfr~age_group_id+sex_id+haqi, df=df, subset=train)
+plot(tree.sepsis_train)
+text(tree.sepsis_train, pretty = 0)
+
+
+# doesnt work
+# tree.pred = predict(tree.sepsis_train, df[-train,], type="tree")
+# with(df[-train,], table(tree.pred, High))
