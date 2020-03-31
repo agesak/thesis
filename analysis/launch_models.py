abb811f22b1d1cba904e8785bb1349daa6f3aa15
Kareha Agesa <agesak@uw.edu>
2020-03-31 14:32:38 -0700

    omg it works

--- analysis/launch_models.py
+++ analysis/launch_models.py
@@ -1,29 +1,52 @@
-from datetime import datetime
+import pandas as pd
+import datetime
 import itertools
 import argparse
 
 from cod_prep.utils.misc import print_log_message
+from cod_prep.claude.claude_io import makedirs_safely
 from mcod_prep.utils.mcod_cluster_tools import submit_mcod
 from thesis_utils.modeling import read_in_data, create_train_test, str2bool
 
 
+# function to read in all model outputs/precision metrics
+# then put in table? or some easy way to compare them
+# then compare to find best model parameters and save
+
 class ModelLauncher():
 
-    def __init__(self, int_cause, run_filters):
+    model_dict = {"rf": "RandomForestClassifier"}
+
+    def __init__(self, run_filters):
         self.run_filters = run_filters
         self.test = self.run_filters["test"]
         self.phase = self.run_filters["phase"]
-        self.int_cause = int_cause
-        self.description = "{:%Y_%m_%d}".format(datetime.datetime.now())
-        if self.test:
-            self.description += "_test"
+        self.int_cause = self.run_filters["int_cause"]
+        if self.run_filters["description"] == "":
+            self.description = "{:%Y_%m_%d}".format(datetime.datetime.now())
+            if self.test:
+                self.description += "_test"
+        else:
+            self.description = self.run_filters["description"]
         self.model_dir = f"/ihme/cod/prep/mcod/process_data/x59/thesis/{self.description}"
+        # print(self.test, type(self.test), self.phase, self.int_cause, self.description, self.model_dir)
 
-    def random_forest_params():
+    def random_forest_params(model):
+        assert model == "RandomForestClassifier", "wrong model type"
 
-        clf__estimator__n_estimators = [200, 500]
-        clf__estimator__max_features = ["auto", "sqrt"]
-        clf__estimator__max_depth = [1, 2] + [None]
+        df = pd.read_csv("/homes/agesak/thesis/maps/parameters.csv")
+        # this syntax is not interchangeable when theres
+        # only 1 value in the column
+        # also it's ugly :(
+        clf__estimator__n_estimators = df.loc[df[
+            f"{model}"] == "clf__estimator__n_estimators",
+            f"{model}_value"].str.split(",")[0]
+        clf__estimator__max_features = df.loc[df[
+            f"{model}"] == "clf__estimator__max_features",
+            f"{model}_value"].tolist()
+        clf__estimator__max_depth = df.loc[df[
+            f"{model}"] == "clf__estimator__max_depth",
+            f"{model}_value"].tolist()
         keys = "clf__estimator__n_estimators", "clf__estimator__max_features", "clf__estimator__max_depth"
         params = [dict(zip(keys, combo)) for combo in itertools.product(
             clf__estimator__n_estimators, clf__estimator__max_features,
@@ -31,9 +54,18 @@ class ModelLauncher():
 
         return params
 
+    def format_params(param):
+        # three bc 3 keys.. may change based on model? - could create model_type:param_number dictionary
+        assert len(param) == 3, "error.. more than one set of params"
+        # turn all params into a single "_" separated string for argparse
+        param = "_".join([str(x) for x in list(param.values())])
+        return param
+
     def create_training_data(self):
+        makedirs_safely(self.model_dir)
         df = read_in_data(self.int_cause)
-        train_df, test_df = create_train_test(df, test=self.test)
+        train_df, test_df = create_train_test(
+            df, test=self.test, int_cause=self.int_cause)
         print_log_message("writing train/test to df")
         train_df.to_csv(f"{self.model_dir}/train_df.csv", index=False)
         test_df.to_csv(f"{self.model_dir}/test_df.csv", index=False)
@@ -42,22 +74,28 @@ class ModelLauncher():
 
         params = [model_param, model, self.model_dir, self.int_cause]
         # idk how to differentiate names
-        jobname = f"{model}_{self.int_cause}"
-        worker = f"/homes/agesakk/thesis/analysis/run_models.py"
+        jobname = f"{model}_{self.int_cause}_{model_param}"
+        worker = f"/homes/agesak/thesis/analysis/run_models.py"
         submit_mcod(jobname, "python", worker, cores=2, memory="6G",
                     params=params, verbose=True, logging=True,
                     jdrive=False, queue="i.q")
 
     def launch(self):
         if self.phase == "train_test":
-            create_train_test()
+            self.create_training_data()
 
         if self.phase == "launch_model":
+            # add argument for model type and dictionary to map to these full names to loop over
+            # have all option
             for model in ["RandomForestClassifier"]:
                 if model == "RandomForestClassifier":
-                    params = ModelLauncher.random_forest_params()
-                    for param in params:
-                        self.launch_model(model, param)
+                    print_log_message("launching Random Forest")
+                    params = ModelLauncher.random_forest_params(model)
+                    print_log_message(
+                        f"{len(params)} sets of model parameters")
+                    for parameter in params:
+                        param = ModelLauncher.format_params(parameter)
+                        self.launch_models(model, param)
 
 
 if __name__ == "__main__":
@@ -65,11 +103,20 @@ if __name__ == "__main__":
     parser.add_argument(
         "--phase", help="", required=True,
         choices=["train_test", "launch_model"])
-    # https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
-    # this prbly doesnt work
-    parser.add_argument("--test", type=str2bool,
+    parser.add_argument("--test", type=str2bool, nargs="?",
                         const=True, default=False,
                         help="Activate nice mode.")
+    parser.add_argument(
+        "--int_cause", help="", required=True,
+        choices=["x59", "y34"])
+    # parser.add_argument(
+    #     "--model_type", help="", type="str"
+    # )
+    # not required
+    parser.add_argument(
+        "description", help='model run; if "launch_model" then date is appended',
+        type=str
+    )
 
     args = parser.parse_args()
     print(vars(args))
--- analysis/run_models.py
+++ analysis/run_models.py
@@ -7,6 +7,7 @@ from cod_prep.utils.misc import print_log_message
 from thesis_utils.grid_search import ClfSwitcher
 
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import precision_score, recall_score, make_scorer
 from sklearn.model_selection import GridSearchCV
@@ -21,25 +22,45 @@ and whether or not this should be applied to all evaluation metrics
 """
 
 
-def main(model_params, model, model_dir, int_cause):
+def format_params(model, param):
+
+    df = pd.read_csv("/homes/agesak/thesis/maps/parameters.csv")
+    params = dict(zip(df[f"{model}"].unique().tolist(), param.split("_")))
+    int_cols = df.loc[df[f"{model}_dtype"] ==
+                      "int", f"{model}"].unique().tolist()
+    # certain parameters must be integers
+    for int_col in int_cols:
+        params[int_col] = [int(params[int_col])]
+    # but all parameters must be lists
+    for col in np.setdiff1d(df[f"{model}"].unique().tolist(), int_cols):
+        params[col] = [params[col]]
+    return params
+
+
+def main(param, model, model_dir, int_cause):
 
     train_df = pd.read_csv(f"{model_dir}/train_df.csv")
     # test_df = pd.read_csv(f"{model_dir}/test_df.csv")
 
-    model_params = ast.literal_eval(model_params)
+    # model_params = ast.literal_eval(model_params)
+
+    model_params = format_params(model, param)
+
+    print(model_params)
 
     pipeline = Pipeline([
         ("bow", CountVectorizer(lowercase=False)),
         ("clf", ClfSwitcher())
     ])
 
-    model_params.update({"clf__estimator": [eval(model)]})
+    model_params.update({"clf__estimator": [eval(model)()]})
     custom_scorer = make_scorer(
         precision_score, greater_is_better=True, average="micro")
-    gscv = GridSearchCV(pipeline, model_params, cv=5,
+    gscv = GridSearchCV(pipeline, model_params, cv=2,
                         scoring=custom_scorer, n_jobs=-1, verbose=6)
     grid_results = gscv.fit(train_df["cause_info"], train_df["cause_id"])
-    grid_results.best_params_
+    # save this in maybe a df? and also save value of scoring metric to append all together and compare
+    print(grid_results.best_params_)
 
 
 if __name__ == '__main__':
@@ -48,124 +69,125 @@ if __name__ == '__main__':
     model = str(sys.argv[2])
     model_dir = str(sys.argv[3])
     int_cause = str(sys.argv[4])
+    print(model_params, model, model_dir, int_cause)
 
     main(model_params, model, model_dir, int_cause)
 
 
-pipeline = Pipeline([
-    ("bow", CountVectorizer(lowercase=False)),
-    ("clf", ClfSwitcher())
-])
-
-# naive bayes, random forest, svm, GBT
-parameters = [
-    # {
-    #     "clf__estimator": [MultinomialNB()]
-    # },
-    {
-        # https://towardsdatascience.com/hyperparameter-tuning-the-random-forest-in-python-using-scikit-learn-28d2aa77dd74
-        "clf__estimator": [RandomForestClassifier()],
-        "clf__estimator__n_estimators": [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
-        "clf__estimator__max_features": ["auto", "sqrt"],
-        "clf__estimator__max_depth": [int(x) for x in np.linspace(10, 110, num=11)] + [None],
-        "clf__estimator__min_samples_split": [2, 5, 10],
-        "clf__estimator__min_samples_leaf": [1, 2, 4],
-        "clf__estimator__bootstrap": [True, False],
-        # "clf__estimator__n_jobs":[-1]
-    }
-    # {
-    #     "clf__estimator": [SVC()]
-    # },
-    #     {
-    #     "clf__estimator":[GradientBoostingClassifier()]
-    # }
-
-]
-
-
-# chad thinks i should be using precision, accuracy, ccc, and ccscfma here (or the one that is most important which might be cccsfma?)
-custom_scorer = make_scorer(
-    precision_score, greater_is_better=True, average="micro")
-gscv = GridSearchCV(pipeline, parameters, cv=5,
-                    scoring=custom_scorer, n_jobs=-1, verbose=6)
-# this takes literal years..
-# i wonder if i could parallelize this by model and write the outputs somewhere...
-# it's SVC that takes forever... (and maybe random forest?)
-grid_results = gscv.fit(train_df["cause_info"], train_df["cause_id"])
-
-# then pick best model parameters for each classifier - other example might be better for this
-# want to output this as a table with metrics and model parameters..
-grid_result.best_params_
-grid_result.cv_results_
-
-# need to store these parameters as objects
-# should this be train_df?
-causes = test_df.cause_id.unique()
-probs = np.random.dirichlet([1] * len(causes), 500)
-# cause order for probabilities based off position
-cause_order = dict(zip(list(range(0, 18, 1)), list(causes)))
-
-
-# now dirichlet and resampling
-# idk why this doesnt work - lots of rows with na cause ids
-nrows = len(test_df)
-test1 = pd.DataFrame(
-    {"cause_id": nrows * [np.NaN], "cause_info": nrows * [np.NaN]})
-K = []
-for value in probs[0]:
-    index_val = list(probs[0]).index(value)
-    K = K + [value]
-    change = test1.sample(frac=value, replace=False).index
-    test1.loc[change, "cause_id"] = cause_order[index_val]
-
-K = []
-for value in probs[0]:
-    index_val = list(probs[0]).index(value)
-    K = K + [value]
-    ugh = test1.sample(frac=value, replace=False)
-    ugh["cause_id"] = cause_order[index_val]
-    test1.update(ugh)
-
-len(test1.loc[test1.cause_id.notnull()]) / len(test1)
-
-# sample to get cause info col too
-
-# same thing
-# cv = CountVectorizer(lowercase=False)
-# tf = cv.fit_transform(train_df["cause_info"])
-# clf = MultinomialNB().fit(tf, train_df["cause_id"])
-# new_counts = cv.transform(test_df["cause_info"])
-# predicted = clf.predict(new_counts)
-# test_df["predicted"] = predicted
-# np.mean(test_df.predicted == test_df.cause_id)
-
-
-# naive bayes, random forest, svm, GBT, Neural network
-# use Grid Search to choose between many parameters
-# text_clf = Pipeline([
+# pipeline = Pipeline([
 #     ("bow", CountVectorizer(lowercase=False)),
-#     ("naive_bayes", MultinomialNB())
-#     ])
-# text_clf.fit(train_df["cause_info"], train_df["cause_id"])
-# predicted = text_clf.predict(test_df["cause_info"])
-# np.mean(predicted == test_df["cause_id"])
+#     ("clf", ClfSwitcher())
+# ])
+
+# # naive bayes, random forest, svm, GBT
+# parameters = [
+#     # {
+#     #     "clf__estimator": [MultinomialNB()]
+#     # },
+#     {
+#         # https://towardsdatascience.com/hyperparameter-tuning-the-random-forest-in-python-using-scikit-learn-28d2aa77dd74
+#         "clf__estimator": [RandomForestClassifier()],
+#         "clf__estimator__n_estimators": [int(x) for x in np.linspace(start=200, stop=2000, num=10)],
+#         "clf__estimator__max_features": ["auto", "sqrt"],
+#         "clf__estimator__max_depth": [int(x) for x in np.linspace(10, 110, num=11)] + [None],
+#         "clf__estimator__min_samples_split": [2, 5, 10],
+#         "clf__estimator__min_samples_leaf": [1, 2, 4],
+#         "clf__estimator__bootstrap": [True, False],
+#         # "clf__estimator__n_jobs":[-1]
+#     }
+#     # {
+#     #     "clf__estimator": [SVC()]
+#     # },
+#     #     {
+#     #     "clf__estimator":[GradientBoostingClassifier()]
+#     # }
+
+# ]
 
 
-# from sklearn.base import BaseEstimator
-# class ClfSwitcher(BaseEstimator):
-# for building a pipeline
-# https://stackoverflow.com/questions/50285973/pipeline-multiple-classifiers - better way of viewing model results
-# http://www.davidsbatista.net/blog/2018/02/23/model_optimization/
+# # chad thinks i should be using precision, accuracy, ccc, and ccscfma here (or the one that is most important which might be cccsfma?)
+# custom_scorer = make_scorer(
+#     precision_score, greater_is_better=True, average="micro")
+# gscv = GridSearchCV(pipeline, parameters, cv=5,
+#                     scoring=custom_scorer, n_jobs=-1, verbose=6)
+# # this takes literal years..
+# # i wonder if i could parallelize this by model and write the outputs somewhere...
+# # it's SVC that takes forever... (and maybe random forest?)
+# grid_results = gscv.fit(train_df["cause_info"], train_df["cause_id"])
+
+# # then pick best model parameters for each classifier - other example might be better for this
+# # want to output this as a table with metrics and model parameters..
+# grid_result.best_params_
+# grid_result.cv_results_
+
+# # need to store these parameters as objects
+# # should this be train_df?
+# causes = test_df.cause_id.unique()
+# probs = np.random.dirichlet([1] * len(causes), 500)
+# # cause order for probabilities based off position
+# cause_order = dict(zip(list(range(0, 18, 1)), list(causes)))
 
 
-# Eval - precision, sensitivity, chance-corrected concordance, CCCSMFA
-# precision - TP/(TP+FP)
-precision_score(y_true=test_df.cause_id,
-                y_pred=test_df.predicted, average="micro")
+# # now dirichlet and resampling
+# # idk why this doesnt work - lots of rows with na cause ids
+# nrows = len(test_df)
+# test1 = pd.DataFrame(
+#     {"cause_id": nrows * [np.NaN], "cause_info": nrows * [np.NaN]})
+# K = []
+# for value in probs[0]:
+#     index_val = list(probs[0]).index(value)
+#     K = K + [value]
+#     change = test1.sample(frac=value, replace=False).index
+#     test1.loc[change, "cause_id"] = cause_order[index_val]
 
-# sensitivity (also known as recall) - TP/(TP+FN)
-# in a multi-class setting micro-averaged precision and recall are always the same.
-recall_score(y_true=test_df.cause_id,
-             y_pred=test_df.predicted, average="micro")
+# K = []
+# for value in probs[0]:
+#     index_val = list(probs[0]).index(value)
+#     K = K + [value]
+#     ugh = test1.sample(frac=value, replace=False)
+#     ugh["cause_id"] = cause_order[index_val]
+#     test1.update(ugh)
 
-# starting to sample from dirichlet
+# len(test1.loc[test1.cause_id.notnull()]) / len(test1)
+
+# # sample to get cause info col too
+
+# # same thing
+# # cv = CountVectorizer(lowercase=False)
+# # tf = cv.fit_transform(train_df["cause_info"])
+# # clf = MultinomialNB().fit(tf, train_df["cause_id"])
+# # new_counts = cv.transform(test_df["cause_info"])
+# # predicted = clf.predict(new_counts)
+# # test_df["predicted"] = predicted
+# # np.mean(test_df.predicted == test_df.cause_id)
+
+
+# # naive bayes, random forest, svm, GBT, Neural network
+# # use Grid Search to choose between many parameters
+# # text_clf = Pipeline([
+# #     ("bow", CountVectorizer(lowercase=False)),
+# #     ("naive_bayes", MultinomialNB())
+# #     ])
+# # text_clf.fit(train_df["cause_info"], train_df["cause_id"])
+# # predicted = text_clf.predict(test_df["cause_info"])
+# # np.mean(predicted == test_df["cause_id"])
+
+
+# # from sklearn.base import BaseEstimator
+# # class ClfSwitcher(BaseEstimator):
+# # for building a pipeline
+# # https://stackoverflow.com/questions/50285973/pipeline-multiple-classifiers - better way of viewing model results
+# # http://www.davidsbatista.net/blog/2018/02/23/model_optimization/
+
+
+# # Eval - precision, sensitivity, chance-corrected concordance, CCCSMFA
+# # precision - TP/(TP+FP)
+# precision_score(y_true=test_df.cause_id,
+#                 y_pred=test_df.predicted, average="micro")
+
+# # sensitivity (also known as recall) - TP/(TP+FN)
+# # in a multi-class setting micro-averaged precision and recall are always the same.
+# recall_score(y_true=test_df.cause_id,
+#              y_pred=test_df.predicted, average="micro")
+
+# # starting to sample from dirichlet
